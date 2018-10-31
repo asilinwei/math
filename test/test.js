e73469f3efc35b5f76d82c30a74691d5c3bf5ded
asilinwei <1021069119@qq.com>
2018-10-31 16:23:35 +0800

    test: add test case about.pow.js
    Signed-off-by: asilinwei <1021069119@qq.com>

--- test/test.js
+++ test/test.js
@@ -139,3 +139,117 @@ describe('Math._toHex', function() {
       should.equal(Math._toHex(-Infinity), (-Infinity).toString(16));
    });
 });
+
+// Test Math._pow
+describe('Math._pow', function() {
+
+   it('should return Infinity when the base is Infinity, the exponent is positive number', function() {
+      should.equal(Math._pow(Infinity, 2), Infinity);
+      should.equal(Math._pow(Infinity, 1 / 3), Infinity);
+      should.equal(Math._pow(Infinity, 2.34), Infinity);
+      should.equal(Math._pow(Infinity, Number.MIN_VALUE), Infinity);
+      should.equal(Math._pow(Infinity, Number.MAX_VALUE), Infinity);
+      should.equal(Math._pow(Infinity, Number.MAX_SAFE_INTEGER), Infinity);
+      should.equal(Math._pow(Infinity, Infinity), Infinity);
+   });
+
+   it('should return zero when the base is Infinity, the exponent is negative number', function() {
+      should.equal(Math._pow(Infinity, -2), 0);
+      should.equal(Math._pow(Infinity, -1 / 3), 0);
+      should.equal(Math._pow(Infinity, -2.34), 0);
+      should.equal(Math._pow(Infinity, -Number.MIN_VALUE), 0);
+      should.equal(Math._pow(Infinity, -Number.MAX_VALUE), 0);
+      should.equal(Math._pow(Infinity, Number.MIN_SAFE_INTEGER), 0);
+      should.equal(Math._pow(Infinity, -Infinity), 0);
+   });
+
+   it('should return Infinity when the absolute value of the base is >= 1, the exponent is Infinity', function() {
+      should.equal(Math._pow(1, Infinity), Infinity);
+      should.equal(Math._pow(-1, Infinity), Infinity);
+      should.equal(Math._pow(1.34, Infinity), Infinity);
+      should.equal(Math._pow(-1.34, Infinity), Infinity);
+      should.equal(Math._pow(2, Infinity), Infinity);
+      should.equal(Math._pow(-2, Infinity), Infinity);
+      should.equal(Math._pow(Number.MAX_VALUE, Infinity), Infinity);
+      should.equal(Math._pow(-Number.MAX_VALUE, Infinity), Infinity);
+      should.equal(Math._pow(Number.MAX_SAFE_INTEGER, Infinity), Infinity);
+      should.equal(Math._pow(-Number.MAX_SAFE_INTEGER, Infinity), Infinity);
+      should.equal(Math._pow(Infinity, Infinity), Infinity);
+      should.equal(Math._pow(-Infinity, Infinity), Infinity);
+   });
+
+   it('should return zero when the absolute value of the base is < 1, the exponent is Infinity', function() {
+      should.equal(Math._pow(1 / 3, Infinity), 0);
+      should.equal(Math._pow(-1 / 3, Infinity), 0);
+      should.equal(Math._pow(Number.MIN_VALUE, Infinity), 0);
+      should.equal(Math._pow(-Number.MIN_VALUE, Infinity), 0);
+      should.equal(Math._pow(0.64, Infinity), 0);
+      should.equal(Math._pow(-0.64, Infinity), 0);
+   });
+
+   it('should return NaN when the base or the exponent can not be converted to number', function() {
+      Math._pow('a', 2).should.be.NaN;
+      Math._pow(2, 'a').should.be.NaN;
+      Math._pow(2).should.be.NaN;
+      Math._pow().should.be.NaN;
+   });
+
+   it('should return number when the base and the exponent can be converted to number', function() {
+      should.equal(Math._pow('2', '3'), 8);
+   });
+
+   it('should return NaN when the base is -2.34, the exponent is -2.34 or 2.34', function() {
+      Math._pow(-2.34, -2.34).should.be.NaN;
+      Math._pow(-2.34, 2.34).should.be.NaN;
+   });
+
+   it('should return the result of calling Math.pow', function() {
+      should.equal(Math._pow(2.34, -2.34), Math.pow(2.34, -2.34));
+      should.equal(Math._pow(2.34, 2.34), Math.pow(2.34, 2.34));
+      should.equal(Math._pow(0.34, -0.34), Math.pow(0.34, -0.34));
+      should.equal(Math._pow(0.34, 0.34), Math.pow(0.34, 0.34));
+      should.equal(Math._pow(2, 3), Math.pow(2, 3));
+      should.equal(Math._pow(-2, 3), Math.pow(-2, 3));
+      should.equal(Math._pow(2, -3), Math.pow(2, -3));
+      should.equal(Math._pow(-2, -3), Math.pow(-2, -3));
+      should.equal(Math._pow(0, 0), Math.pow(0, 0));
+      should.equal(Math._pow(0, 1), Math.pow(0, 1));
+      should.equal(Math._pow(0, -1), Math.pow(0, -1));
+      should.equal(Math._pow(1, 0), Math.pow(1, 0));
+      should.equal(Math._pow(-1, 0), Math.pow(-1, 0));
+      should.equal(Math._pow(Number.MIN_VALUE, 0), Math.pow(Number.MIN_VALUE, 0));
+      should.equal(Math._pow(-Number.MIN_VALUE, 0), Math.pow(-Number.MIN_VALUE, 0));
+      should.equal(Math._pow(0, Number.MIN_VALUE), Math.pow(0, Number.MIN_VALUE));
+      should.equal(Math._pow(Number.MIN_VALUE, 2), Math.pow(Number.MIN_VALUE, 2));
+      should.equal(Math._pow(-Number.MIN_VALUE, 2), Math.pow(-Number.MIN_VALUE, 2));
+      should.equal(Math._pow(2, Number.MIN_VALUE), Math.pow(2, Number.MIN_VALUE));
+      should.equal(Math._pow(Number.MIN_VALUE, -2), Math.pow(Number.MIN_VALUE, -2));
+      should.equal(Math._pow(-Number.MIN_VALUE, -2), Math.pow(-Number.MIN_VALUE, -2));
+      should.equal(Math._pow(0, Number.MAX_VALUE), Math.pow(0, Number.MAX_VALUE));
+      should.equal(Math._pow(Number.MAX_VALUE, 0), Math.pow(Number.MAX_VALUE, 0));
+      should.equal(Math._pow(-Number.MAX_VALUE, 0), Math.pow(-Number.MAX_VALUE, 0));
+      should.equal(Math._pow(2, Number.MAX_VALUE), Math.pow(2, Number.MAX_VALUE));
+      should.equal(Math._pow(Number.MAX_VALUE, 2), Math.pow(Number.MAX_VALUE, 2));
+      should.equal(Math._pow(-Number.MAX_VALUE, 2), Math.pow(-Number.MAX_VALUE, 2));
+      should.equal(Math._pow(-2, Number.MAX_VALUE), Math.pow(-2, Number.MAX_VALUE));
+      should.equal(Math._pow(Number.MAX_VALUE, -2), Math.pow(Number.MAX_VALUE, -2));
+      should.equal(Math._pow(-Number.MAX_VALUE, -2), Math.pow(-Number.MAX_VALUE, -2));
+      should.equal(Math._pow(0, Number.MAX_SAFE_INTEGER), Math.pow(0, Number.MAX_SAFE_INTEGER));
+      should.equal(Math._pow(Number.MAX_SAFE_INTEGER, 0), Math.pow(Number.MAX_SAFE_INTEGER, 0));
+      should.equal(Math._pow(-Number.MAX_SAFE_INTEGER, 0), Math.pow(-Number.MAX_SAFE_INTEGER, 0));
+      should.equal(Math._pow(2, Number.MAX_SAFE_INTEGER), Math.pow(2, Number.MAX_SAFE_INTEGER));
+      should.equal(Math._pow(Number.MAX_SAFE_INTEGER, 2), Math.pow(Number.MAX_SAFE_INTEGER, 2));
+      should.equal(Math._pow(-Number.MAX_SAFE_INTEGER, 2), Math.pow(-Number.MAX_SAFE_INTEGER, 2));
+      should.equal(Math._pow(-2, Number.MAX_SAFE_INTEGER), Math.pow(-2, Number.MAX_SAFE_INTEGER));
+      should.equal(Math._pow(Number.MAX_SAFE_INTEGER, -2), Math.pow(Number.MAX_SAFE_INTEGER, -2));
+      should.equal(Math._pow(-Number.MAX_SAFE_INTEGER, -2), Math.pow(-Number.MAX_SAFE_INTEGER, -2));
+   });
+
+   it('should return NaN when the base is negative number, the exponent is Number.MIN_VALUE', function() {
+      Math._pow(-2, Number.MIN_VALUE).should.be.NaN;
+      Math._pow(-1 / 3, Number.MIN_VALUE).should.be.NaN;
+      Math._pow(-2.34, Number.MIN_VALUE).should.be.NaN;
+   });
+});
+
+console.log(Math._pow(-Infinity,Number.MAX_SAFE_INTEGER));
